9044bb2b16decfc951c97c4c3f64b01e9b701fd3
openworld2526-arch <openworld2526@gmail.com>
2026-03-22 23:25:01 +0100

    Update bot and owner numbers in settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,12 +14,12 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '212679226276',
+ pairingNumber : '212679226276',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
- nameown : 'NOUREDDINE',
- nomerown : '212717457920',
+ nomorwa : '212679226276',
+ nameown : 'OUSSAMA ZORO',
+ nomerown : '212679226276',
  packname : 'sticker by ',
  author : 'SILANA AI',
  namebot : '乂 SILANA AI',
